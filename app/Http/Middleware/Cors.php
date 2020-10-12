30916adaa83fe1dcbd9451599393d3c03c2f49b8
Abraham <adolfo.91.garcia@gmail.com>
2020-10-12 13:42:42 -0300

    funcionando los cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {   
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, X-Auth-Token, Country');
+        header('Access-Control-Allow-Credentials: true');
+
+        if($request->getMethod() == 'OPTIONS') {
+            exit(0);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(['middleware' => 'cors'], function() {
+    /* Auth Routes */
+    Route::post('login', 'AuthController@login');
 
-Route::post('/login', 'AuthController@login');
+});
